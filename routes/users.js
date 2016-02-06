40fe881a77cbe1a9319ca343a4978997ae0056b1
Aodhagán <aodhaganc@gmail.com>
2016-02-06 13:27:10 +0000

    Code: add messages to jade input

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('user', { user: req.user });
+  res.render('user', { user: req.user, messages: req.flash('info') });
 });
 
 module.exports = router;
