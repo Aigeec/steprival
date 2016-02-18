5943e488caf16cb891f91867e47fdefa35f5bcae
Aodhagan Collins <aodhaganc@gmail.com>
2016-02-18 22:26:51 +0000

    Code: refactor routes/user to always return users page to support angular 2 html5 routing

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/:userid', function(req, res, next) {
+var goToUserPage = function(req, res, next) {
   console.log('userid: ', req.params.userid);
-  if(req.params.userid === req.user._id){
+  if (req.params.userid === req.user._id) {
     res.render('user', { user: req.user, messages: req.flash('info') });
   } else {
     req.flash('info', 'For now you may only view your own profile');
     res.redirect('/user/' + req.user._id);
   }
-});
+};
+
+/* GET users listing. */
+router.get('/:userid', goToUserPage);
+router.get('/:userid/*', goToUserPage);
 
 module.exports = router;
