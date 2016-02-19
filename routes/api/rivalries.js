b6e8ca48731898851d4c174cb628da423462ccdf
Aodhagan Collins <aodhaganc@gmail.com>
2016-02-19 00:21:33 +0000

    Code: Use REST api for users and rivalries

--- routes/api/rivalries.js
+++ routes/api/rivalries.js
@@ -2,21 +2,13 @@ var express = require('express');
 var router = express.Router();
 var db = require('../../startup/mongodb').get();
 var Promise = require('bluebird');
-var https = require('https');
+var rest = require('./rest');
 
 router.use(function (req, res, next) {
-  req.collection = 'user';
+  req.collection = 'rivalries';
   next();
 });
 
-router.get('/:userId/steps',
-  function (req, res, next) {
-
-  }
-);
-
 router.use(rest);
-
-
 
 module.exports = router;
