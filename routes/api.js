7cd20b77a7a91f71eb733fede6ac1fc30219eefe
Aodhagán <aodhaganc@gmail.com>
2016-03-03 22:45:09 +0000

    Code: fix parameter order

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,7 @@ var processMessages = function(messages, res){
     respondOnCompletion(promises, res);
 }
 
-var respondOnCompletion = function(res, promises){
+var respondOnCompletion = function(promises, res){
   Promise.all(promises).then(
     function(){
       res.sendStatus(204);
