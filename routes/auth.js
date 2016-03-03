1a3b3a9be9469ff1be5ee3162de7a812746ebb49
Aodhagan Collins <aodhaganc@gmail.com>
2016-03-03 22:43:04 +0000

    Code: add add Subscrition on fitbit auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,30 @@
 var passport = require('passport');
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 router.get('/fitbit', passport.authenticate('fitbit', { scope: ['profile', 'activity'] }));
 
-var directToUsersPage = function(req, res) {
+var directToUsersPage = function (req, res) {
   res.redirect('/user/' + req.user._id);
+};
+
+var addFitbitSubscription = function (req, res) {
+  request
+    .post('/1/user/-/apiSubscriptions/1.json')
+    .auth(null, null, true, user.accessToken)
+    .on('response', function (err, fitbitRes, body) {
+      if (err) {
+        console.log('Error: Could not add subscription. ', err);
+      }
+
+      directToUsersPage(req, res);
+    });
 };
 
 router.get('/fitbit/callback',
   passport.authenticate('fitbit', { failureRedirect: '/login' }),
-  directToUsersPage
+  addFitbitSubscription
 );
 
 router.get('/misfit', passport.authenticate('misfit'));
